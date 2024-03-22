67f8d267d18d0b20036284c0b4c7fe1dbcfeb081
mikiprogrammer79 <115552497+mikiprogrammer79@users.noreply.github.com>
2024-03-22 10:15:43 +0000

    Create a coin toss program

---
+++ day4/heads_tails.py
@@ -0,0 +1,11 @@
+# A virtual coin toss program. It will randomly tell the user "Heads" or "Tails".
+
+import random
+
+random_coin = random.randint(0, 1)
+
+if random_coin == 0:
+    print("Heads")
+else:
+    print("Tails")
+
