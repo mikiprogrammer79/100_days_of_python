0b67ab72b91bbdaf85839a350f54fd67ebbda2cc
mikiprogrammer79 <115552497+mikiprogrammer79@users.noreply.github.com>
2024-03-25 14:30:04 +0000

    Create hangman.py pseudocode and welcome to the program

---
+++ day7/hangman.py
@@ -0,0 +1,25 @@
+# Hangman Game
+
+words = ["greenkeeper", "beekeeper", "furniture", "diamond", "coffee"]
+
+# Welcome to the game
+print('''
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/                       
+      ''')
+
+# Start
+
+# Generate a random word
+
+# Generate as many blanks as letters in word
+
+# Ask the user to guess a letter
+
+# Control Flow and logic of the game
