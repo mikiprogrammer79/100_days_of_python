c59b54771e02e7e407a6f795e55fcfa1bf038310
mikiprogrammer79 <115552497+mikiprogrammer79@users.noreply.github.com>
2024-04-01 10:40:57 +0100

    Create coffee machine python data file

---
+++ day15/cm_data.py
@@ -0,0 +1,35 @@
+# Coffee Machine Data
+
+resources = {
+    "water": 300, # ml
+    "milk": 200, # ml
+    "coffee": 100, # g
+}
+
+coffee_expenses = {
+    "espresso": {
+        "water": 50,
+        "milk": 0,
+        "coffee": 18,
+        "price": 1.50,
+    },
+    "latte": {
+        "water": 200,
+        "milk": 150,
+        "coffee": 24,
+        "price": 2.50,
+    },
+    "cappuccino": {
+        "water": 250,
+        "milk": 100,
+        "coffee": 24,
+        "price": 3.00,
+    },
+}
+
+money = {
+    "penny": 0.01,
+    "nickel": 0.05,
+    "dime": 0.10,
+    "quarter": 0.25,
+}
