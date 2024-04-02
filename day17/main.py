a8e6a480c261a89556d6cf52c36938da04f54b47
mikiprogrammer79 <115552497+mikiprogrammer79@users.noreply.github.com>
2024-04-02 15:16:08 +0100

    Create question_bank

---
+++ day17/main.py
@@ -0,0 +1,9 @@
+from question import Question
+from data import question_data
+
+question_bank = []
+
+for i in range(0, len(question_data)):
+    question_bank.append(Question(question_data[i]["text"], question_data[i]["answer"]))
+
+print(question_bank)
