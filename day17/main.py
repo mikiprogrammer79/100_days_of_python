b5515d96efc14c05e7ec13587bdda69c1eb0a4ce
mikiprogrammer79 <115552497+mikiprogrammer79@users.noreply.github.com>
2024-04-02 16:31:56 +0100

    Create main.py program

--- day17/main.py
+++ day17/main.py
@@ -1,9 +1,14 @@
 from question import Question
 from data import question_data
+from quiz import Quiz
 
 question_bank = []
 
 for i in range(0, len(question_data)):
     question_bank.append(Question(question_data[i]["text"], question_data[i]["answer"]))
 
-print(question_bank)
+quiz = Quiz(question_bank)
+
+while quiz.still_has_questions():
+
+    quiz.ask_question()
