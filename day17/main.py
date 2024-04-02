4327273beb523f0bfd5ca924b5aee393a6a5ecde
mikiprogrammer79 <115552497+mikiprogrammer79@users.noreply.github.com>
2024-04-02 17:28:26 +0100

    Add question_data2 as option 'B' to append to question_bank list

--- day17/main.py
+++ day17/main.py
@@ -1,14 +1,27 @@
 from question import Question
-from data import question_data
+from data import question_data, question_data2
 from quiz import Quiz
 
 question_bank = []
 
-for i in range(0, len(question_data)):
-    question_bank.append(Question(question_data[i]["text"], question_data[i]["answer"]))
+prompt = input("What quiz number do you want to do? Type 'A' or 'B':  ").lower()
+
+if prompt == "a":
+
+    for i in range(0, len(question_data)):
+        question_bank.append(Question(question_data[i]["text"], question_data[i]["answer"]))
+
+elif prompt == "b":
+
+    for i in range(0, len(question_data2["results"])):
+        question_bank.append(Question(question_data2["results"][i]["question"], question_data2["results"][i]["correct_answer"]))
+
 
 quiz = Quiz(question_bank)
 
 while quiz.still_has_questions():
 
     quiz.ask_question()
+
+
+
