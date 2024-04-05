6134de31ee681947a3b60ea8a6b274fe2276dacf
mikiprogrammer79 <115552497+mikiprogrammer79@users.noreply.github.com>
2024-04-05 10:57:40 +0100

    Create snake body and move forward

---
+++ day20_21/main.py
@@ -0,0 +1,50 @@
+# Snake Game
+
+from turtle import Turtle, Screen
+
+
+screen = Screen()
+
+screen.setup(width=600, height=600) # Screen size
+
+screen.bgcolor("black") # Screen background color
+
+screen.title("The Python Game") # Screen Title
+
+tail = [(0, 0), (-20, 0), (-40, 0)] # List of segments coor
+
+snake_segments = []
+
+# Create a snake body (3 squares 20x20px)
+
+for x_y in tail:
+    segment = Turtle(shape= "square")
+    segment.penup()
+    segment.color("white")
+    segment.goto(x_y)
+    snake_segments.append(segment)
+
+# Move snake
+
+snake_segments.reverse()
+
+game_alive = True
+
+while game_alive:
+    
+    for seg in snake_segments:
+        seg.forward(20)
+
+
+# Move snake: Animating the snake segments
+
+
+
+
+
+
+
+
+
+screen.exitonclick() # Exit screen on click
+
