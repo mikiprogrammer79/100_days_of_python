23022650968fea8793306af8f0131d7dc6371fdc
mikiprogrammer79 <115552497+mikiprogrammer79@users.noreply.github.com>
2024-04-07 10:49:30 +0100

    Change shapesize streching length and width half size

--- day20_21/food.py
+++ day20_21/food.py
@@ -6,12 +6,12 @@ class Food(Turtle):
         super().__init__()
         self.shape("circle")
         self.penup()
-        self.shapesize(stretch_len=8.5, stretch_wid=8.5)
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
         self.color("yellow")
         self.speed("fastest")
         random_x = random.randint(-250, 250)
         random_y = random.randint(-250, 250)
         self.goto(random_x, random_y)
-        
+
 
     
