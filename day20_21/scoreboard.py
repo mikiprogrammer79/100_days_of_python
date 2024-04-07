c340e60bea78ff51c57b5550ba2735c32226c359
mikiprogrammer79 <115552497+mikiprogrammer79@users.noreply.github.com>
2024-04-07 11:10:17 +0100

    Create class Scoreboard attributes and show_score() method

--- day20_21/scoreboard.py
+++ day20_21/scoreboard.py
@@ -1,3 +1,15 @@
-class Scoreboard:
+from turtle import Screen
+
+
+class Scoreboard(Screen):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.show_score()
+
+    def show_score(self):
+        self.title(f"Score: {self.score}")
+        self.score += 1
 
     
