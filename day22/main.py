4b3081b84f541c733efc53e07383594db2811830
mikiprogrammer79 <115552497+mikiprogrammer79@users.noreply.github.com>
2024-04-08 09:57:33 +0100

    Create the screen

--- day22/main.py
+++ day22/main.py
@@ -1,5 +1,15 @@
 # The Pong Game
+
+from turtle import Turtle, Screen
+
+
 # Create the screen
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black") # Background color
+screen.title("The Pong Game") # Creen window title
+
+
 # Create a move a paddle
 # Create another paddle
 # Create the ball and make it move across the screen
@@ -7,3 +17,6 @@
 # Detect collision with paddle
 # Detect when paddle misses the ball
 # Keep score
+
+
+screen.exitonclick()
