af032ba131d8e36536e585e9a7f24cb8935d16e5
mikiprogrammer79 <115552497+mikiprogrammer79@users.noreply.github.com>
2024-04-08 10:40:33 +0100

    Move paddle up/down add/sub 20 steps

--- day22/paddle.py
+++ day22/paddle.py
@@ -1,5 +1,7 @@
 from turtle import Turtle
 
+X_COR = 350
+
 class Paddle(Turtle):
     def __init__(self):
         super().__init__()
@@ -7,12 +9,12 @@ class Paddle(Turtle):
         self.shape("square")
         self.shapesize(stretch_wid=5, stretch_len=1)
         self.penup()
-        self.goto(350, 0)
+        self.goto(X_COR, 0)
 
     def move_up(self):
-        self.setheading(90)
-        self.forward(20)
+        new_y = self.ycor() + 20
+        self.goto(X_COR, new_y)
 
     def move_down(self):
-        self.setheading(270)
-        self.forward(20)
+        new_y = self.ycor() - 20
+        self.goto(X_COR, new_y)
