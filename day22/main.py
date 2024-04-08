84206ddf65044d2179d5e1c1a3bb567f3316d6dc
mikiprogrammer79 <115552497+mikiprogrammer79@users.noreply.github.com>
2024-04-08 09:48:59 +0100

    Create pseudocode

--- day22/main.py
+++ day22/main.py
@@ -1,1 +1,9 @@
 # The Pong Game
+# Create the screen
+# Create a move a paddle
+# Create another paddle
+# Create the ball and make it move across the screen
+# Detect collision with wall and bounce
+# Detect collision with paddle
+# Detect when paddle misses the ball
+# Keep score
