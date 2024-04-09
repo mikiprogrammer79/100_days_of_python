0430abdab4396cce2e36d15de8a2f3c2406fcf16
mikiprogrammer79 <115552497+mikiprogrammer79@users.noreply.github.com>
2024-04-09 09:00:51 +0100

    Setup screen

--- day23/main.py
+++ day23/main.py
@@ -1,2 +1,16 @@
 # The Turtle Crossing Game
 
+import time
+from turtle import Turtle, Screen
+
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.title("The Turtle Crossing Game")
+screen.tracer(0)
+
+game_alive = True
+while game_alive:
+    time.sleep(0.1)
+    screen.update()
+    
