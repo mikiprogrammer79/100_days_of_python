c12d9a976c599267ab9339df2eb9b0ae6154b48b
mikiprogrammer79 <115552497+mikiprogrammer79@users.noreply.github.com>
2024-04-09 09:36:38 +0100

    Setup screen event listeners and move turtle on key

--- day23/main.py
+++ day23/main.py
@@ -4,7 +4,7 @@ import time
 from turtle import Turtle, Screen
 from car_manager import CarManager
 from player import Player
-
+from scoreboard import Scoreboard
 
 
 screen = Screen()
@@ -12,7 +12,20 @@ screen.setup(width=600, height=600)
 screen.title("The Turtle Crossing Game")
 screen.tracer(0)
 
+
+player = Player()
+
+# Screen event listeners
+screen.listen()
+screen.onkey(fun=player.move, key="Up")
+
+
 game_alive = True
 while game_alive:
     time.sleep(0.1)
     screen.update()
+
+
+
+
+screen.exitonclick()
--- day23/player.py
+++ day23/player.py
@@ -1,6 +1,19 @@
+from turtle import Turtle
+
+
 STARTING_POSITION = (0, -200)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 200
+UP = 90
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.penup()
+        self.setheading(UP)
+        self.goto(STARTING_POSITION)
+
+    def move(self):
+        self.forward(MOVE_DISTANCE)
+
