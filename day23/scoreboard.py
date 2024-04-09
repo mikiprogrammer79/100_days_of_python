1aeda9c2ec3ad0b890146447fbb067a5377f5a0a
mikiprogrammer79 <115552497+mikiprogrammer79@users.noreply.github.com>
2024-04-09 16:19:54 +0100

    Show score

--- day23/scoreboard.py
+++ day23/scoreboard.py
@@ -1,4 +1,19 @@
-FONT = ("Courier", 24, "normal" )
+from turtle import Turtle
 
-class Scoreboard:
-    pass
+FONT = ("Courier", 24, "normal" )
+SCORE_POSITION = (-250, 200)
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.hideturtle()
+        self.penup()
+        self.goto(SCORE_POSITION)
+        
+
+    def show_score(self, level):
+        self.clear()
+        self.write(f"Level: {level}", font=FONT)
+
+
+        
+
