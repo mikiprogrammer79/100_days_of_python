fa1013a7e645de3cfc9cacccade409f06390b7c7
mikiprogrammer79 <115552497+mikiprogrammer79@users.noreply.github.com>
2024-04-10 10:57:33 +0100

    Open data.txt and read content as high_score

--- day24/snake/scoreboard.py
+++ day24/snake/scoreboard.py
@@ -7,7 +7,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -21,11 +22,11 @@ class Scoreboard(Turtle):
         self.score += 1
 
     def reset(self):
-        if self.score > self.high_score:
-            self.high_score = self.score
+        if self.score - 1 > self.high_score:
+            self.high_score = self.score - 1
         self.score = 0
         self.show_score()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("Game Over", align=ALIGMENT, font=FONT)
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("Game Over", align=ALIGMENT, font=FONT)
