eef45c46186a40516d022da98b39e4d44f829fa7
mikiprogrammer79 <115552497+mikiprogrammer79@users.noreply.github.com>
2024-04-11 14:30:57 +0100

    Create a CSV file to count squirrels in Central Park

--- day25/main.py
+++ day25/main.py
@@ -21,7 +21,7 @@ import pandas
 
 data = pandas.read_csv("day25/weather_data.csv")  #type(data) -> DataFrame
 
-print(data["temp"])   # temperature data: just name the name of the column
+# print(data["temp"])   # temperature data: just name the name of the column
 
 data_dict = data.to_dict() # Convert DataFrame to a dictionary
 
@@ -32,14 +32,14 @@ data_list = data['temp'].to_list() # Convert Series to a list
 
 # Calculate average temperature
 avg_temp = data['temp'].mean()
-print(f"Average temperature: {round(avg_temp, 2)}")
+# print(f"Average temperature: {round(avg_temp, 2)}")
 
 # Get the maximum temperature
 max_temp = data['temp'].max()
-print(f"Maximum temperature: {max_temp}")
+# print(f"Maximum temperature: {max_temp}")
 
 # Get minimum temperature
-print(data.temp.min()) # DataFrame.column_name -> Series
+# print(data.temp.min()) # DataFrame.column_name -> Series
 
 # Get data of one row. Thursday
 thursday = data[data.day == "Thursday"]
@@ -54,7 +54,7 @@ max_temp_day = data[data.temp == max_temp]
 # Convert Monday temperature from Celsius to Fahrenheit (C * 9/5 + 32)
 monday_celsius = data.temp[data.day == 'Monday']
 monday_fahrenheit = monday_celsius * 9 / 5 + 32
-print(monday_fahrenheit)
+# print(monday_fahrenheit)
 
 # Create a new DataFrame from scratch
 new_data_dict = {
@@ -67,5 +67,21 @@ data_frame = pandas.DataFrame(new_data_dict)
 # print(data_frame)
 
 # Create a dataframe into a csv file
-data_frame.to_csv("day25/student_scores.csv")
+# data_frame.to_csv("day25/student_scores.csv")
+
+# Count squirrels in Central Park (New York). https://thesquirrelcensus.com && 
+# https://data.cityofnewyork.us (download CSV data)
+# Create a CSV file 'squirrel_count.csv' Fur Color, Count
+data = pandas.read_csv("day25/2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+
+fur_color_counts = data["Primary Fur Color"].value_counts()
+
+new_dataframe = pandas.DataFrame(fur_color_counts)
+
+new_dataframe.to_csv("day25/squirrel_count.csv")
+
+
+
+
+
  
