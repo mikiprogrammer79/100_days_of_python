d74f01715b584b4c9142f19238ed83fc8a35f019
mikiprogrammer79 <115552497+mikiprogrammer79@users.noreply.github.com>
2024-04-16 10:49:51 +0100

    Add entry

---
+++ day27/mile_km_converter.py
@@ -0,0 +1,20 @@
+# Miles to Kilometers Converter
+
+from tkinter import *
+
+
+window = Tk()
+window.title("Mile to Km Converter")
+window.minsize(width=250, height=150)
+
+# Entry Miles
+entry = Entry(width=10)
+entry.pack()
+
+
+
+
+
+
+
+window.mainloop()
