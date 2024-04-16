a43875a0d8f9adb80cba65f16a7919d3ec6a7f29
mikiprogrammer79 <115552497+mikiprogrammer79@users.noreply.github.com>
2024-04-16 10:36:33 +0100

    Create GUI window

---
+++ mile_km_converter.py
@@ -0,0 +1,17 @@
+# Miles to Kilometers Converter
+
+from tkinter import *
+
+
+window = Tk()
+window.title("Miles to Kilometers Converter")
+window.minsize(width=300, height=200)
+
+
+
+
+
+
+
+
+window.mainloop()
