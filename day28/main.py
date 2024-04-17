a8a958b3fbe1944fc87af3b6a27029757e902a16
mikiprogrammer79 <115552497+mikiprogrammer79@users.noreply.github.com>
2024-04-17 11:29:16 +0100

    Change argument 'y' to move the canvas text to the center of the tomato

--- day28/main.py
+++ day28/main.py
@@ -24,7 +24,7 @@ window.config(padx=100, pady=50)
 canvas = Canvas(width=200, height=224)   # Create a Canvas Widget with W and H similar to the image
 tomato_img = PhotoImage(file="day28/tomato.png") # Read the image file
 canvas.create_image(100, 112, image=tomato_img)   # Create image with 1/2canvas_width and 1/2canvas_Height to put the image in the center
-canvas.create_text(100, 112, text="00:00")  # Create text in the center of the canvas
+canvas.create_text(100, 130, text="00:00")  # Create text in the center of the canvas
 canvas.pack()
 
 
