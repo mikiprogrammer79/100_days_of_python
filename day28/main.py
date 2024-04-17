d09b233817e033b86bc4fa3750679e14cc7c1c81
mikiprogrammer79 <115552497+mikiprogrammer79@users.noreply.github.com>
2024-04-17 12:03:33 +0100

    Create Labels and buttons

--- day28/main.py
+++ day28/main.py
@@ -21,13 +21,23 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
-canvas = Canvas(width=200, height=224, bg=GREEN, highlightthickness=0)   # Create a Canvas Widget with W and H similar to the image
+label = Label(text="Timer", font=(FONT_NAME, 30, "normal"), fg=GREEN, bg=YELLOW, highlightthickness=0)
+label.grid(column=1, row=0)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)   # Create a Canvas Widget with W and H similar to the image
 tomato_img = PhotoImage(file="day28/tomato.png") # Read the image file
 canvas.create_image(100, 112, image=tomato_img)   # Create image with 1/2canvas_width and 1/2canvas_Height to put the image in the center
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))  # Create text in the center of the canvas
-canvas.pack()
+canvas.grid(column=1, row=1)
 
+start_button = Button(text="Start")
+start_button.grid(column=0, row=2)
 
+reset_button = Button(text="Reset")
+reset_button.grid(column=2, row=2)
+
+checkmark = Label(text="✔", fg=GREEN, font=(FONT_NAME, 24, "bold"), bg=YELLOW)
+checkmark.grid(column=1, row=3)
 
 
 window.mainloop()
