e8d4075764b7c0efc2f01521329c898b31ca7e51
mikiprogrammer79 <115552497+mikiprogrammer79@users.noreply.github.com>
2024-04-17 11:44:40 +0100

    Remove border with highlightthickness set to zero

--- day28/main.py
+++ day28/main.py
@@ -21,7 +21,7 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
-canvas = Canvas(width=200, height=224, bg=GREEN)   # Create a Canvas Widget with W and H similar to the image
+canvas = Canvas(width=200, height=224, bg=GREEN, highlightthickness=0)   # Create a Canvas Widget with W and H similar to the image
 tomato_img = PhotoImage(file="day28/tomato.png") # Read the image file
 canvas.create_image(100, 112, image=tomato_img)   # Create image with 1/2canvas_width and 1/2canvas_Height to put the image in the center
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))  # Create text in the center of the canvas
