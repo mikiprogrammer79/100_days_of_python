9b8f2515549e3e192bd9952a17a0443179863d9f
mikiprogrammer79 <115552497+mikiprogrammer79@users.noreply.github.com>
2024-04-18 10:38:07 +0100

    Create buttons and add padding to the window

--- day29/main.py
+++ day29/main.py
@@ -8,7 +8,7 @@ from tkinter import *
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
 # Canvas padding 20px W: 200 H: 200
 img = PhotoImage(file="day29/logo.png")
@@ -20,18 +20,23 @@ canvas.grid(column=1, row=0)
 website = Label(text="Website:")
 website.grid(column=0, row=1)
 website_entry = Entry(width=35)
-website_entry.grid(column=1, row=1)
+website_entry.grid(column=1, row=1, sticky=W)
 
 user_name = Label(text="Email/Username:")
 user_name.grid(column=0, row=2)
 user_entry = Entry(width=35)
-user_entry.grid(column=1, row=2)
+user_entry.grid(column=1, row=2, sticky=W)
 
 password = Label(text="Password:")
 password.grid(column=0, row=3)
 password_entry = Entry(width=21)
-password_entry.grid(column=1, row=3)
+password_entry.grid(column=1, row=3, sticky=W)
 
+# Buttons
+generate_button = Button(text="Generate Password")
+generate_button.grid(column=1, row=3, sticky=E)
 
+add_button = Button(text="Add", width=36)
+add_button.grid(column=1, row=4)
 
 window.mainloop()
