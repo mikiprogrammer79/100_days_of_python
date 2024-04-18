b103c3e9b4ea9d9df8d74404e97fdc8c0bd88af2
mikiprogrammer79 <115552497+mikiprogrammer79@users.noreply.github.com>
2024-04-18 10:07:18 +0100

    Create Labels and Entries

--- day29/main.py
+++ day29/main.py
@@ -14,8 +14,23 @@ window.config(padx=20, pady=20)
 img = PhotoImage(file="day29/logo.png")
 canvas = Canvas(width=200, height=200, highlightthickness=3, highlightbackground="black")
 canvas.create_image(100, 100, image=img)
-canvas.pack()
+canvas.grid(column=1, row=0)
 
+# Labels and Entries
+website = Label(text="Website:")
+website.grid(column=0, row=1)
+website_entry = Entry(width=35)
+website_entry.grid(column=1, row=1)
+
+user_name = Label(text="Email/Username:")
+user_name.grid(column=0, row=2)
+user_entry = Entry(width=35)
+user_entry.grid(column=1, row=2)
+
+password = Label(text="Password:")
+password.grid(column=0, row=3)
+password_entry = Entry(width=21)
+password_entry.grid(column=1, row=3)
 
 
 
